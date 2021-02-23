3edea6cf55bfa35b5245c3216507042faf7de112
Valcin <valcin.david@hotmail.fr>
2021-02-23 19:26:35 +0100

    updates

--- GW_NN/admeq.py
+++ GW_NN/admeq.py
@@ -72,45 +72,45 @@ some_3d_time_dependent_pde_solution, _ = _solve_3dspatial_temporal(
 # specify the ODE system and its parameters
 def ADM(K11,K12,K13,K22,K23,K33,K,a,b1,b2,b3,G11,G12,G13,G22,G23,G33, r, theta, phi, t):
 
-	# define the metric
-	G11 = 1./(1 - (2*M)/r)
-	G12 = 0
-	G13 = 0
-	G21 = 0
-	G22 = 1./(1 - (2*M)/r) * r**2
-	G23 = 0
-	G31 = 0
-	G32 = 0
-	G33 = 1./(1 - (2*M)/r) * r**2 * torch.sin(theta)**2
+	# define the contravariant metric
+	G_11 = 1./G11
+	G_12 = 1./G12
+	G_13 = 1./G13
+	G_21 = 1./G21
+	G_22 = 1./G22
+	G_23 = 1./G23
+	G_31 = 1./G31
+	G_32 = 1./G32
+	G_33 = 1./G33
 
-	# define the christoffel coefficients
-	C111 = 1/2.*(1./G11)*(diff(G11, r) + diff(G11, r) - diff(G11, r))
-	C112 = 1/2.*(1./G11)*(diff(G11, theta) + diff(G12, r) - diff(G12, r))
-	C113 = 1/2.*(1./G11)*(diff(G13, phi) + diff(G13, r) - diff(G13, r))
-	C121 = 1/2.*(1./G11)*(diff(G12, r) + diff(G11, theta) - diff(G21, r))
-	C122 = 1/2.*(1./G11)*(diff(G12, theta) + diff(G12, theta) - diff(G22, r))
-	C123 = 1/2.*(1./G11)*(diff(G12, phi) + diff(G13, theta) - diff(G23, r))
-	C131 = 1/2.*(1./G11)*(diff(G13, r) + diff(G11, phi) - diff(G31, r))
-	C132 = 1/2.*(1./G11)*(diff(G13, theta) + diff(G12, phi) - diff(G32, r))
-	C133 = 1/2.*(1./G11)*(diff(G13, phi) + diff(G13, phi) - diff(G33, r))
-	C211 = 1/2.*(1./G22)*(diff(G21, r) + diff(G21, r) - diff(G11, theta))
-	C212 = 1/2.*(1./G22)*(diff(G21, theta) + diff(G22, r) - diff(G12, theta))
-	C213 = 1/2.*(1./G22)*(diff(G21, phi) + diff(G23, r) - diff(G13, theta))
-	C221 = 1/2.*(1./G22)*(diff(G22, r) + diff(G21, theta) - diff(G21, theta))
-	C222 = 1/2.*(1./G22)*(diff(G22, theta) + diff(G22, theta) - diff(G22, theta))
-	C223 = 1/2.*(1./G22)*(diff(G22, phi) + diff(G23, theta) - diff(G23, theta))
-	C231 = 1/2.*(1./G22)*(diff(G23, r) + diff(G21, phi) - diff(G31, theta))
-	C232 = 1/2.*(1./G22)*(diff(G23, theta) + diff(G22, phi) - diff(G32, theta))
-	C233 = 1/2.*(1./G22)*(diff(G23, phi) + diff(G23, phi) - diff(G33, theta))
-	C311 = 1/2.*(1./G33)*(diff(G31, r) + diff(G31, r) - diff(G11, phi))
-	C312 = 1/2.*(1./G33)*(diff(G31, theta) + diff(G32, r) - diff(G12, phi))
-	C313 = 1/2.*(1./G33)*(diff(G31, phi) + diff(G33, r) - diff(G13, phi))
-	C321 = 1/2.*(1./G33)*(diff(G32, r) + diff(G31, theta) - diff(G21, phi))
-	C322 = 1/2.*(1./G33)*(diff(G32, theta) + diff(G32, theta) - diff(G22, phi))
-	C323 = 1/2.*(1./G33)*(diff(G32, phi) + diff(G33, theta) - diff(G23, phi))
-	C331 = 1/2.*(1./G33)*(diff(G33, r) + diff(G31, phi) - diff(G31, phi))
-	C332 = 1/2.*(1./G33)*(diff(G33, theta) + diff(G32, phi) - diff(G32, phi))
-	C333 = 1/2.*(1./G33)*(diff(G33, phi) + diff(G33, phi) - diff(G33, phi))
+	# define the christoffel coefficients only accounting for G11, G22, G33 (TO MODIFY)
+	C111 = 1/2.*G_11*(diff(G11, r) + diff(G11, r) - diff(G11, r))
+	C112 = 1/2.*G_11*(diff(G11, theta) + diff(G12, r) - diff(G12, r))
+	C113 = 1/2.*G_11*(diff(G13, phi) + diff(G13, r) - diff(G13, r))
+	C121 = 1/2.*G_11*(diff(G12, r) + diff(G11, theta) - diff(G21, r))
+	C122 = 1/2.*G_11*(diff(G12, theta) + diff(G12, theta) - diff(G22, r))
+	C123 = 1/2.*G_11*(diff(G12, phi) + diff(G13, theta) - diff(G23, r))
+	C131 = 1/2.*G_11*(diff(G13, r) + diff(G11, phi) - diff(G31, r))
+	C132 = 1/2.*G_11*(diff(G13, theta) + diff(G12, phi) - diff(G32, r))
+	C133 = 1/2.*G_11*(diff(G13, phi) + diff(G13, phi) - diff(G33, r))
+	C211 = 1/2.*G_22*(diff(G21, r) + diff(G21, r) - diff(G11, theta))
+	C212 = 1/2.*G_22*(diff(G21, theta) + diff(G22, r) - diff(G12, theta))
+	C213 = 1/2.*G_22*(diff(G21, phi) + diff(G23, r) - diff(G13, theta))
+	C221 = 1/2.*G_22*(diff(G22, r) + diff(G21, theta) - diff(G21, theta))
+	C222 = 1/2.*G_22*(diff(G22, theta) + diff(G22, theta) - diff(G22, theta))
+	C223 = 1/2.*G_22*(diff(G22, phi) + diff(G23, theta) - diff(G23, theta))
+	C231 = 1/2.*G_22*(diff(G23, r) + diff(G21, phi) - diff(G31, theta))
+	C232 = 1/2.*G_22*(diff(G23, theta) + diff(G22, phi) - diff(G32, theta))
+	C233 = 1/2.*G_22*(diff(G23, phi) + diff(G23, phi) - diff(G33, theta))
+	C311 = 1/2.*G_33*(diff(G31, r) + diff(G31, r) - diff(G11, phi))
+	C312 = 1/2.*G_33*(diff(G31, theta) + diff(G32, r) - diff(G12, phi))
+	C313 = 1/2.*G_33*(diff(G31, phi) + diff(G33, r) - diff(G13, phi))
+	C321 = 1/2.*G_33*(diff(G32, r) + diff(G31, theta) - diff(G21, phi))
+	C322 = 1/2.*G_33*(diff(G32, theta) + diff(G32, theta) - diff(G22, phi))
+	C323 = 1/2.*G_33*(diff(G32, phi) + diff(G33, theta) - diff(G23, phi))
+	C331 = 1/2.*G_33*(diff(G33, r) + diff(G31, phi) - diff(G31, phi))
+	C332 = 1/2.*G_33*(diff(G33, theta) + diff(G32, phi) - diff(G32, phi))
+	C333 = 1/2.*G_33*(diff(G33, phi) + diff(G33, phi) - diff(G33, phi))
  
 
 	# define Ricci iteration
@@ -158,7 +158,7 @@ def ADM(K11,K12,K13,K22,K23,K33,K,a,b1,b2,b3,G11,G12,G13,G22,G23,G33, r, theta, 
 
 
 	# Compute total Ricci scalar
-	R = 
+	R = G_11*R11 + G_22*R22 + G_33*R33
 
 	#define source terms (here vacuum)
 	rho = 0
@@ -169,31 +169,20 @@ def ADM(K11,K12,K13,K22,K23,K33,K,a,b1,b2,b3,G11,G12,G13,G22,G23,G33, r, theta, 
 	S11 = 0
 	S12 = 0
 	S13 = 0
+	S21 = 0
 	S22 = 0
 	S23 = 0
+	S31 = 0
+	S32 = 0
 	S33 = 0
 
-	#define the lapse and shift
-	return [R + K**2 + (K11**2 + K12**2 + K13**2 + K22**2 + K23**2 + K33**2) - 16*math.pi*rho,
-	diff((K11-G11*K), r) + diff((K12-G12*K), theta) + diff((K13-G13*K), phi) - 8*math.pi*S1,
-	diff((K22-G22*K), theta) + diff((K23-G23*K), phi) - 8*math.pi*S2,
-	diff((K33-G33*K), phi) - 8*math.pi*S3,
-	-diff(G11,t) -2*a*K11 + diff(b1,r) + diff(b1,r),
-	-diff(G12,t) -2*a*K12 + diff(b2,r) + diff(b1,theta),
-	-diff(G13,t) -2*a*K13 + diff(b3,r) + diff(b1,phi),
-	-diff(G22,t) -2*a*K22 + diff(b2,theta) + diff(b2,theta),
-	-diff(G23,t) -2*a*K23 + diff(b3,theta) + diff(b2,phi),
-	-diff(G33,t) -2*a*K33 + diff(b3,phi) + diff(b3,phi),
-	-diff(K11,t) + a(R11 -2*(K11*K11) + K*K11) - diff(diff(a,r),r) - 8*math.pi*a*(S11 - 1/2.*G11*(S-rho)) + b1*diff(K11,r) + b2*diff(K11,theta) + b3*diff(K11,phi) + K11*diff(b1,r) + K12*diff(b2,r) + K13*diff(b3,r) + K11*diff(b1,r),
-	-diff(K12,t) + a(R12 -2*(K11*K12 + K12*K22) + K*K12) - diff(diff(a,theta),r) - 8*math.pi*a*(S12 - 1/2.*G12*(S-rho)) + b1*diff(K12,r) + b2*diff(K12,theta) + b3*diff(K12,phi) + K11*diff(b1,theta) + K12*diff(b2,theta) + K13*diff(b3,theta) + K12*diff(b1,r) + K22*diff(b2,r),
-	-diff(K13,t) + a(R13 -2*(K11*K13 + K12*K23 + K13*K33) + K*K13) - diff(diff(a,phi),r) - 8*math.pi*a*(S13 - 1/2.*G13*(S-rho)) + b1*diff(K13,r) + b2*diff(K13,theta) + b3*diff(K13,phi) + K11*diff(b1,phi) + K12*diff(b2,phi) + K13*diff(b3,phi) + K13*diff(b1,r) + K23*diff(b2,r) + K33*diff(b3,theta),
-	-diff(K22,t) + a(R22 -2*(K22*K22) + K*K22) - diff(diff(a,theta),theta) - 8*math.pi*a*(S22 - 1/2.*G22*(S-rho)) + b1*diff(K22,r) + b2*diff(K22,theta) + b3*diff(K22,phi) + K22*diff(b2,theta) + K23*diff(b3,theta) + K12*diff(b1,theta) + K22*diff(b2,theta),
-	-diff(K23,t) + a(R23 -2*(K22*K23 + K23*K33) + K*K23) - diff(diff(a,phi),theta) - 8*math.pi*a*(S23 - 1/2.*G23*(S-rho)) + b1*diff(K23,r) + b2*diff(K23,theta) + b3*diff(K23,phi) + K22*diff(b2,phi) + K23*diff(b3,phi) + K13*diff(b1,theta) + K23*diff(b2,theta) + K33*diff(b3,theta),
-	-diff(K33,t) + a(R33 -2*(K33*K33) + K*K33) - diff(diff(a,phi),phi) - 8*math.pi*a*(S33 - 1/2.*G33*(S-rho)) + b1*diff(K33,r) + b2*diff(K33,theta) + b3*diff(K33,phi) + K33*diff(b3,phi) + K13*diff(b1,phi) + K23*diff(b2,phi) + K33*diff(b3,phi),
-	-K + K11 + K22 + K33]
+	#define contravariant Ricci tensor
+	K_11 = (1./G11)*()
+
+	return [R + K**2 - K11*(1./G11)*(1./G11)*K11 - K12*(1./G11)*(1./G22)*K11]
 
 # specify the initial conditions
-init_vals_lv = [IVP(t_0=0.0, x_0=0.0), 
+init_vals_lv = [IVP(t_0=0.0, x_0=0.0),#K11 
 IVP(t_0=0.0, x_0=0.0),  #K12
 IVP(t_0=0.0, x_0=0.0), #K13
 IVP(t_0=0.0, x_0=0.0), #K22
@@ -201,19 +190,15 @@ IVP(t_0=0.0, x_0=0.0), #K23
 IVP(t_0=0.0, x_0=0.0), #K33
 IVP(t_0=0.0, x_0=0.0), #K
 IVP(t_0=0.0, x_0=lambda r: torch.sqrt(1 - 2*M/r)), #a
-# ~IVP(t_0=0.0, x_0=0.0), #a
 IVP(t_0=0.0, x_0=0.0), #b1
 IVP(t_0=0.0, x_0=0.0), #b2
 IVP(t_0=0.0, x_0=0.0), #b3
-IVP(t_0=0.0, x_0=lambda r: 1./(1 - 2*M/r)), #G11
-# ~IVP(t_0=0.0, x_0=0.0), #G11
+IVP(t_0=0.0, x_0=lambda r: 1./(1 - (2*M)/r)), #G11
 IVP(t_0=0.0, x_0=0.0), #G12
 IVP(t_0=0.0, x_0=0.0), #G13
-IVP(t_0=0.0, x_0=lambda r: r**2), #G22
-# ~IVP(t_0=0.0, x_0=0.0), #G22
+IVP(t_0=0.0, x_0=lambda r: 1./(1 - (2*M)/r) * r**2), #G22
 IVP(t_0=0.0, x_0=0.0), #G23
-IVP(t_0=0.0, x_0=lambda r: r**2 * torch.sin(theta)**2), #G33
-# ~IVP(t_0=0.0, x_0=0.0), #G33
+IVP(t_0=0.0, x_0=lambda r: 1./(1 - (2*M)/r) * r**2 * torch.sin(theta)**2), #G33
 ]
 
 # specify the network to be used to approximate each dependent variable
